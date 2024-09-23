3a72040cf9d04d8fe99e56708896be37290a69ab
japan-da-man <suzukinao.nao.nao.nao@gmail.com>
2024-09-23 21:51:49 +0900

    feat: translate your site

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,7 +26,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en','fr'],
   },
 
   presets: [
@@ -86,6 +86,9 @@ const config: Config = {
         },
         {
           type: 'docsVersionDropdown',
+        },
+        {
+          type: 'localeDropdown',
         }
       ],
     },
