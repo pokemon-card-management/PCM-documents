625d513508da303aa23c55d99a4c685f47233c6b
japan-da-man <suzukinao.nao.nao.nao@gmail.com>
2024-09-23 22:32:41 +0900

    feat: gh-pagesを追加

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'pokemon-card-management', // Usually your GitHub org/user name.
   projectName: 'PCM-documents', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
