c4c3f92a1dad17333983753464a63977bac01f74
japan-da-man <suzukinao.nao.nao.nao@gmail.com>
2024-09-23 16:08:57 +0900

    fix: gitignoreを反映

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/naosuzuki/Documents/PokemonCardManagement/PCM-documents/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/naosuzuki/Documents/PokemonCardManagement/PCM-documents/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/naosuzuki/Documents/PokemonCardManagement/PCM-documents/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/naosuzuki/Documents/PokemonCardManagement/PCM-documents/src/css/custom.css"),
-];
