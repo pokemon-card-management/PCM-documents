a7fd415a902a5397d2799cf3bfde181ce7ef6892
japan-da-man <suzukinao.nao.nao.nao@gmail.com>
2024-09-24 23:38:57 +0900

    fix: url変更

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   url: 'https://github.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/pages/pokemon-card-management/PCM-documents/',
+  baseUrl: '/PCM-documents/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
